377fdeeaa5d702b65ce03c4246bc329a053a549c
sonukumar766 <sonuk.cse@gmail.com>
2022-04-06 02:58:06 +0530

    upadate template

--- temp1.cpp
+++ temp1.cpp
@@ -15,7 +15,7 @@ class vector{
     int dotProduct(vector &v){
         int d=0;
         for(int i=0;i<size;i++){
-            d+=this->arr[i]*arr[i];
+            d+=this->arr[i]* v.arr[i];
 
        }return d;
     }
@@ -24,13 +24,13 @@ int main(){
     vector v1(3);
     v1.arr[0]=9;
     v1.arr[1]=0;
-    v1.arr[2]=90;
+    v1.arr[2]=6;
 
 
     vector v2(3);
-    v2.arr[0]=9;
-    v2.arr[1]=0;
-    v2.arr[2]=90;
+    v2.arr[0]=6;
+    v2.arr[1]=4;
+    v2.arr[2]=3;
 
     int a=v1.dotProduct(v2);
     cout<<a<<endl;
