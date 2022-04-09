f52c9217bdb4dc331e0015f5694926cc87a3dc55
sonukumar766 <sonuk.cse@gmail.com>
2022-04-09 19:26:31 +0530

    c++ programs

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,15 @@
+// c++ program to add two numbers
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    int sum;
+    cout<<"Enter the value of a:"<<endl;
+    cin>>a;
+    cout<<"Enter the value of b:"<<endl;
+    cin>>b;
+    sum=a+b;
+    cout<<"sum of two number "<<" = "<<sum<<endl;
+    return 0;
+}
---
+++ p2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+//This code is created by Sonu Kumar
+
+/*
+this 
+is the 
+multiple line 
+comments 
+in c++
+*/
+
+int main(){
+
+    cout<<"This is sonu kumar"<<endl;
+    return 0;
+}
