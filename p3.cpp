c0a24d81c0f07e4050d0c1ec957250208244faea
sonukumar766 <sonuk.cse@gmail.com>
2022-04-14 14:57:55 +0530

    c++ programs

---
+++ p3.cpp
@@ -0,0 +1,16 @@
+//c++ programm to multiply two numbers u7sing function
+#include<iostream>
+using namespace std;
+int mul(int x,int y){
+    return x*y;
+}
+// drive code
+int main(){
+    int x, y;
+    cout<<"Enter the value of x:"<<endl;
+    cin>>x;
+    cout<<"Enter the value of y:";
+    cin>>y;
+    cout<<"sum"<<" = "<<x+y;
+    return 0;
+}
---
+++ p4.cpp
@@ -0,0 +1,15 @@
+// data types in c++
+#include<iostream>
+using namespace std;
+int main(){
+    int a=90;
+    int b=100;
+    cout<<"the value of a is "<<a<<" The value of b is "<<b<<endl;
+    float c=343.778;
+    float d=455.87;
+    char e='s';
+    char f='o';
+    cout<<"The value of c is "<<c<<" The value of d is "<<d<<" The value of e is "<< e<<" The value of f is "<<f;
+
+    return 0;
+}
