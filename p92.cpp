1860f5270c60669223b84ba692d620c4da1e51c6
sonukumar766 <sonuk.cse@gmail.com>
2022-05-19 16:28:19 +0530

    reading unknown number of inputs.

---
+++ p92.cpp
@@ -0,0 +1,15 @@
+// reading unknown number in c++
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int sum=0, value=0;
+
+    while(cin>>value){
+        sum+=value; // same as sum=sum+value;
+    }
+    cout<<" sum = "<<sum<<endl;
+    
+    return 0;
+}
