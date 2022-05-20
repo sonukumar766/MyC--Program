8fd90b8485cc905afd56de747b93d33bbab7dcd1
sonukumar766 <sonuk.cse@gmail.com>
2022-05-20 14:26:11 +0530

    Update p95.cpp

--- p95.cpp
+++ p95.cpp
@@ -1,37 +1,40 @@
-// copy constructor in c++ 
-#include<iostream>
+// copy constructor in c++
+#include <iostream>
 using namespace std;
 
-class Point{
+class Point
+{
     int x;
     int y;
-    public:
-    Point(int x1, int y1){
-        x=x1;
-        y=y1;
+
+public:
+    Point(int x1, int y1)
+    {
+        x = x1;
+        y = y1;
     }
     // copy constructor in c++
-    Point(const Point &p1){
-        x=p1.x;
-        y=p1.y;
-
+    Point(const Point &p1)
+    {
+        x = p1.x;
+        y = p1.y;
     }
-    void displayPoint(){
-        cout<<"x = "<<x<<", y = "<<y<<endl;
+    void displayPoint()
+    {
+        cout << "x = " << x << ", y = " << y << endl;
     }
-
-
 };
 
-int main(){
-    Point p1(12,34); //Normal constructor called here
-    Point p2=p1; // Copy constructor called here
+int main()
+{
+    Point p1(12, 34); // Normal constructor called here
+    Point p2 = p1;    // Copy constructor called here
 
-    cout<<"the point p1 is "<<endl;
+    cout << "the point p1 is " << endl;
     p1.displayPoint();
 
-    cout<<"the copy point p2 is "<<endl;
+    cout << "the copy point p2 is " << endl;
     p2.displayPoint();
-    
+
     return 0;
 }
