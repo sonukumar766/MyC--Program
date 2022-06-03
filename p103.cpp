a905023ed238f0b11fada79c45afe5701efd0349
sonukumar766 <sonuk.cse@gmail.com>
2022-06-03 17:59:14 +0530

    Update p103.cpp

--- p103.cpp
+++ p103.cpp
@@ -1,25 +1,29 @@
 // dynamic memeory allocation using malloc() function
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cout<<"Enter the number og integers: "<<endl;
-    cin>>n;
-    int* ptr = (int*)malloc(n*sizeof(int));
+    cout << "Enter the number og integers: " << endl;
+    cin >> n;
+    int *ptr = (int *)malloc(n * sizeof(int));
 
-    if(ptr==NULL){
-        cout<<"Memory is not available"<<endl;
+    if (ptr == NULL)
+    {
+        cout << "Memory is not available" << endl;
         exit(1);
     }
-    for(int i=0;i<n;i++){
-        cout<<"Enter the integer: ";
-        cin>>*(ptr+i);
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the integer: ";
+        cin >> *(ptr + i);
     }
-    for(int i = 0; i<n; i++){
-        cout<<"The element number "<<(i+1)<<endl;
-        cout<<*(ptr+i)<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "The element number " << (i + 1) << endl;
+        cout << *(ptr + i) << endl;
     }
-    
+
     return 0;
 }
