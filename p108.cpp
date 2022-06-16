f173c4c3f629c16dc331d7ab8d99c86921d5e571
sonukumar766 <sonuk.cse@gmail.com>
2022-06-16 16:30:55 +0530

    Update p108.cpp

--- p108.cpp
+++ p108.cpp
@@ -1,29 +1,33 @@
 // constructor with default arguments in c++
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Simple{
+class Simple
+{
     int data1;
     int data2;
     int data3;
-    public:
-    Simple(int a, int b=6, int c = 9){
-        data1=a;
-        data2=b;
-        data3=c;
-    }
-    void printData(){
-        cout<<"The value of data1, data2 and data3 is "<<data1<<", "<<data2<<" and "<<data3<<endl;
-    }
 
+public:
+    Simple(int a, int b = 6, int c = 9)
+    {
+        data1 = a;
+        data2 = b;
+        data3 = c;
+    }
+    void printData()
+    {
+        cout << "The value of data1, data2 and data3 is " << data1 << ", " << data2 << " and " << data3 << endl;
+    }
 };
 
 // driver code
 
-int main(){
+int main()
+{
 
-    Simple s(1,2);
+    Simple s(1, 2);
     s.printData();
-    
+
     return 0;
 }
