be159e191e0741fb2c7043c93933274cc38b058b
sonukumar766 <sonuk.cse@gmail.com>
2022-06-17 10:42:36 +0530

    c++ program to find the greatest number among the three numbers.

---
+++ p111.cpp
@@ -0,0 +1,24 @@
+// c++ program to finding the greatest number among the three
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n1,n2,n3;
+    cout<<"The first number is :";
+    cin>>n1;
+    cout<<"The second number is: ";
+    cin>>n2;
+    cout<<"THe third number is: ";
+    cin>>n3;
+    if(n1>n2 && n1>n3){
+        cout<<"n1 is greatest."<<endl;
+    }
+    else if(n2>n1 && n2>n3){
+        cout<<"n2 is greatest."<<endl;
+    }
+    else{
+        cout<<"n3 is greatest."<<endl;
+    }
+    
+    return 0;
+}
