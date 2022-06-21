4948bc71f791d661f1584e0b7f58ccc20f36c8a3
sonukumar766 <sonuk.cse@gmail.com>
2022-06-21 12:47:03 +0530

    vector in c++

--- p109.cpp
+++ p109.cpp
@@ -1,31 +1,35 @@
 // sorting the vector in c++
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    vector<int>v;
+    vector<int> v;
     int n;
-    cout<<"Enter the size of the vector: "<<endl;
-    cin>>n;
+    cout << "Enter the size of the vector: " << endl;
+    cin >> n;
     int element;
-    for(int i=0;i<n;i++){
-        cout<<"Enter the element to vector: ";
-        cin>>element;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the element to vector: ";
+        cin >> element;
         v.push_back(element);
     }
-    cout<<"Original vector: ";
-    for(int i=0;i<v.size();i++){
-        cout<<v[i]<<" ";
+    cout << "Original vector: ";
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
     }
-    cout<<endl;
-    cout<<"Sorted vector: ";
+    cout << endl;
+    cout << "Sorted vector: ";
     sort(v.begin(), v.end());
-    for(auto x:v){
-        cout<<x<<" ";
+    for (auto x : v)
+    {
+        cout << x << " ";
     }
-    
+
     return 0;
 }
