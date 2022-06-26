296d8a10e59059846f559cdd4d0410f53d945706
sonukumar766 <sonuk.cse@gmail.com>
2022-06-26 20:38:53 +0530

    reading the sentence in c.

---
+++ p118.c
@@ -0,0 +1,11 @@
+// read the sentence in c++ and print that sentence on the console window
+#include<stdio.h>
+#include<string.h>
+#define MAX 1000
+int main(){
+    char str[100];
+    printf("Enter the sentence: ");
+    fgets(str, MAX, stdin);
+    printf("The enterd sentence is %s", str);
+    return 0;
+}
