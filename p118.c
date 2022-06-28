c669ac24908ddc46799c908338e9d1cee8bebde6
sonukumar766 <sonuk.cse@gmail.com>
2022-06-28 14:56:43 +0530

    Update p118.c

--- p118.c
+++ p118.c
@@ -1,8 +1,9 @@
 // read the sentence in c++ and print that sentence on the console window
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 #define MAX 1000
-int main(){
+int main()
+{
     char str[100];
     printf("Enter the sentence: ");
     fgets(str, MAX, stdin);
