78acaaebbcfe5acf21a72333afb26150ea7cca7e
sonukumar766 <sonuk.cse@gmail.com>
2022-07-27 23:34:30 +0530

    Update p141.c

--- p141.c
+++ p141.c
@@ -1,19 +1,20 @@
 // write c program to convert the lowercase sentence to upper case sentence
-#include<stdio.h>
-#include<ctype.h>
-#include<string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 #define MAX 1000
-int main(){
+int main()
+{
 
     char str[100];
     printf("Enter your sentence: ");
     fgets(str, MAX, stdin);
     printf("The original string is: %s", str);
 
-    for(int i=0;str[i]!='\0';i++){
-        str[i]=toupper(str[i]);
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        str[i] = toupper(str[i]);
     }
     printf("The updated string is: %s", str);
     return 0;
-   
 }
